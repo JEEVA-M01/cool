cf4f01044c7d94dfd84129fbead0f3f2e5506086
JEEVA-M01 <150790709+JEEVA-M01@users.noreply.github.com>
2024-08-06 13:47:32 +0530

    Add files via upload

---
+++ DAY 2/d2_class_01.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const App = () =>
+{
+    const sample ={
+        color:'darkseagreen',
+        textAlign: 'center',
+        fontSize: '50px'
+    };
+
+    
+    return(
+        <div style={sample}>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+export default App;
