2a9540408e39ce7459a7090dfdda5d54bc4693c6
JEEVA-M01 <150790709+JEEVA-M01@users.noreply.github.com>
2024-08-10 00:24:01 +0530

    Add files via upload

---
+++ DAY 1/d1_pah_01.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+//Functional Component 
+const App = ()=>
+{
+    return(
+        <div>
+            <h1>This is Team Functional Component</h1>
+        </div>
+    )
+}
+export default App;
